acdebc334cd5c8c3936a67a18176a48fd3be0d05
Alex5X5 <alex.25.lehnert@gmail.com>
2025-04-29 08:25:38 +0200

    bla

--- game/Programm.cs
+++ game/Programm.cs
@@ -1,5 +1,9 @@
 ﻿using ShGame.game.Client.Rendering;
+using ShGame.game.Net;
 using ShGame.game.Util;
+
+using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 using System.Windows.Forms;
 namespace ShGame.game;
@@ -7,14 +11,39 @@ namespace ShGame.game;
 public static class Programm {
 
 	[STAThread]
-	public static void Main() {
-		//Programm3.Main_();
-		//return;
-		//Logging.DisableColors();
-		//RendererGl rd = new();
-		//Client2 c = new();
-		//return;
-		Application.EnableVisualStyles();
+	public static void Main(string[] args) {
+        //Programm3.Main_();
+        //return;
+        //Logging.DisableColors();
+        //RendererGl rd = new();
+        //Client2 c = new();
+        //return;
+
+        List<string> args_ = args.ToList<string>();
+        bool noGui = args_.Contains("-nogui");
+        if (args_.Contains("--server")) {
+            new Thread(
+                () => {
+                    //Console.WriteLine("Initial Screen: ip="+address+" port="+port);
+                    IPAddress? address = null;
+                    int port = 0;
+                    try {
+                        address = IPAddress.Parse(args_.Contains("-ip") ? args_[args_.IndexOf("-ip")+1] : "");
+                    } catch {
+                        address = GameServer.GetLocalIP().MapToIPv4();
+                    }
+                    try {
+                        port = args_.Contains("-port") ? Convert.ToInt32(args_[args_.IndexOf("-port")+1]): -1;
+                    } catch {
+                        port = 5000;
+                    }
+                    _ = new Net.GameServer(address, (uint)port);
+
+                }
+            ).Start();
+
+        }
+        Application.EnableVisualStyles();
 		Application.SetCompatibleTextRenderingDefault(false);
 		//Logging.DisableLog();
 		Console.WriteLine("start");
